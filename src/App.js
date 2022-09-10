07cde2e37ff2887211e0f20acfbc974272cbfe12
Nguyen Van Man <vanman12315@gmail.com>
2022-09-10 15:33:01 +0700

    final

--- src/App.js
+++ src/App.js
@@ -8,6 +8,30 @@ import ModelTemplate from './Templates/ModelTemplate';
 
 function App() {
 
+  let dataGame = localStorage.getItem("dataGame");
+  dataGame = dataGame && JSON.parse(dataGame);
+
+  if(!dataGame) 
+ { 
+  let tempList = [];
+  for(var i = 1; i < 100; i++){
+    let num = (1808*123*i) % Math.pow(10, 4);
+    if (num < Math.pow(10, 4-1)) num += Math.pow(10, 4-1);
+    tempList.push(num);
+}
+  let tempData = {
+      hash: 123,
+      numberOfNum: 4,
+      sizeOfList: 100,
+      data: tempList
+    }
+    localStorage.setItem(
+      "dataGame",
+      JSON.stringify((tempData))
+    );
+  }
+
+
   const [data, setData] = useState(['helo', 'haha']);
 
   return (
--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,4 +1,4 @@
-import { Button } from 'antd';
+import { Button, Input, Popover  } from 'antd';
 import React, {useState} from 'react'
 import {useSelector} from 'react-redux'
 import DigitRoll from '../Components/DigitRoll';
@@ -7,14 +7,62 @@ import RollNumber from '../Components/RollNumber';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 export default function MainPage() {
+  let dataGame = localStorage.getItem("dataGame");
+  dataGame = dataGame && JSON.parse(dataGame);
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const {test} = useSelector(state => state.DataNumberReducer);
+    const [vipData, setVipData] = useState({
+      tiLe: 10,
+      maVe: 1234
+    })
+
+    const [showPop, setShowPop] = useState(false)
+
+    const addVipButton = () => {
+      let tempArray = dataGame;
+      for(var i = 1; i <= (dataGame.sizeOfList * vipData.tiLe / 100); i++){
+        tempArray.data.push(vipData.maVe);
+      }
+      tempArray.sizeOfList = tempArray.data.length;
+      localStorage.setItem(
+        "dataGame",
+        JSON.stringify(tempArray)
+      );
+    }
+
+    const contentPopup = () => {
+      return <div>
+        <p className='m-0'>Tỉ lệ thắng cộng thêm: </p>
+        <Input type='number' value={vipData.tiLe} onChange={(e) => {
+          setVipData({
+            ...vipData,
+            tiLe: e.target.value
+          })
+        }} min={10} max={50}></Input>
+        <p className='m-0'>Mã vé: </p>
+        <Input  type='number' value={vipData.maVe} onChange={(e) => {
+          setVipData({
+            ...vipData,
+            maVe: e.target.value
+          })
+        }}></Input>
+       <div className='flex justify-between mt-5'>
+       <Button type='primary' onClick={() => {
+          console.log(vipData)
+          setShowPop(false);
+          addVipButton();
+        }}>Add</Button>
+        <Button onClick={() => {
+          setShowPop(false)
+        }}>Cancel</Button>
+       </div>
+      </div>
+    }
 
   return (
     <div className='bg-redvintage h-screen'>
-        <h1 className='text-6xl bg-brownvintage text-whitevintage text-center py-5 mb-20 shadow-md'>LUCKY NUMBER</h1>
+        <h1 className='text-6xl bg-brownvintage text-whitevintage text-center py-5 mb-20 shadow-md'>Wheel Of Fortune</h1>
         <RollNumber/>
         <div className='pl-10 fixed bottom-10 left-0'>
           <button
@@ -23,11 +71,22 @@ export default function MainPage() {
            }}
            className='duration-200 bg-brownvintage py-2 px-5 text-white shadow-md rounded-md hover:scale-110 mb-5'>Setting</button>
           <br></br>
+          <Popover open={showPop} content={contentPopup} trigger="click" placement='topRight'>
+          <button 
+          onClick={() => {
+            setShowPop(true)
+          }}
+          className='duration-200 bg-orangevintage py-2 px-5 text-white shadow-md rounded-md hover:scale-110 mb-5'>
+            Add VIP ticket</button>
+          </Popover>
+
+            <br></br>
           <button 
           onClick={() => {
             navigate('/numbers')
           }}
-          className='duration-200 bg-orangevintage py-2 px-5 text-white shadow-md rounded-md hover:scale-110 mb-5'>View list of tickets</button>
+          className='duration-200 bg-orangevintage py-2 px-5 text-white shadow-md rounded-md hover:scale-110 mb-5'>
+            View list of tickets</button>
         </div>
         {/* <RandomRoll/> */}
         {/* <DigitRoll/> */}
